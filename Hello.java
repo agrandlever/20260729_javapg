4ad979857d0af1b47ed517ae25d6479187116cfc
agrandlever <agrandlever@gmail.com>
2026-07-29 15:16:13 +0900

    .gitignoreとHello.javaファイルを追加

---
+++ Hello.java
@@ -0,0 +1,10 @@
+// Helloという名前のクラスを定義する
+public class Hello {
+
+  // Javaプログラムを開始するためのmainメソッド
+  public static void main(String[] args) {
+
+    // ターミナルに文字を表示する
+    System.out.println("おはようございます");
+  }
+}
